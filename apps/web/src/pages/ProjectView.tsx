49fb98225c264b1402d5bf48453210ffa30b4358
Yuvaraj Premlal <100008497+Yuvaraj-Premlal@users.noreply.github.com>
2026-03-22 10:55:38 +0530

    tasktab and projectview upload

--- apps/web/src/pages/ProjectView.tsx
+++ apps/web/src/pages/ProjectView.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { getProject, getTasks, getReviews, getWeeklyReports, getPreReviewBrief, sendNudge, generateWeeklyReport, createTask, createReview } from '../api/projects'
+import { getProject, getTasks, getReviews, getWeeklyReports, getPreReviewBrief, generateWeeklyReport, createReview } from '../api/projects'
 import { api } from '../api/client'
 import TasksTab from './TasksTab'
 
